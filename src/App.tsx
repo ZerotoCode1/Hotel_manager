da31606bbe02291faa20e699bbb56d8f84b827a2
quang tran <chikhoai001@gmail.com>
2024-03-23 09:55:27 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,106 @@
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import {
+  BrowserRouter as Router,
+  Outlet,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n/config";
+import Page404 from "./pages/Page404";
+import { ThemeProvider } from "./providers/ThemeProvider";
+import AuthenticationProvider from "./providers/AuthenticationProvider";
+import { Slide, ToastContainer } from "react-toastify";
+import routes from "./routes/routes";
+import PrivateRoute from "./components/PrivateRoute";
+
+const ErrorFallback = ({ error, resetErrorBoundary }: any) => {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+};
+
+const App = () => {
+  const renderContent = () => {
+    return (
+      <Router>
+        <Routes>
+          {routes.map((route) => {
+            return (
+              <Route
+                key={`${route.path}-layout`}
+                path={route.path}
+                element={
+                  route.isPrivateRoute ? (
+                    <PrivateRoute>
+                      <route.layout>
+                        <Outlet />
+                      </route.layout>
+                    </PrivateRoute>
+                  ) : (
+                    <route.layout>
+                      <Outlet />
+                    </route.layout>
+                  )
+                }
+              >
+                {route.routeChild.map((child, idx) => {
+                  return (
+                    <Route
+                      key={`${child.path}-${idx}`}
+                      path={child.path}
+                      element={
+                        <Suspense fallback={<span>Loading...</span>}>
+                          <ErrorBoundary FallbackComponent={ErrorFallback}>
+                            {child.isPrivateRoute ? (
+                              <PrivateRoute>
+                                <child.component />
+                              </PrivateRoute>
+                            ) : (
+                              <child.component />
+                            )}
+                          </ErrorBoundary>
+                        </Suspense>
+                      }
+                    />
+                  );
+                })}
+              </Route>
+            );
+          })}
+
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </Router>
+    );
+  };
+
+  return (
+    <I18nextProvider i18n={i18n}>
+      <ThemeProvider defaultTheme="light" storageKey="theme">
+        <AuthenticationProvider>
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={true}
+            newestOnTop
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            transition={Slide}
+          />
+          {renderContent()}
+        </AuthenticationProvider>
+      </ThemeProvider>
+    </I18nextProvider>
+  );
+};
+
+export default App;
---
+++ src/layouts/DefaultLayout.tsx
@@ -0,0 +1,9 @@
+interface DefaultLayoutProps {
+  children: React.ReactNode;
+}
+
+const DefaultLayout = (props: DefaultLayoutProps) => {
+  return <div className="component:DefaultLayout">{props.children}</div>;
+};
+
+export default DefaultLayout;
